30a7773dc91b810a621796d0d52f9442ab32aa48
cooolavinash2186 <avnashk80@gmail.com>
2026-07-20 17:09:57 +0530

    initial commit to add file in git

---
+++ src/main/java/com/githubcicd/githubActionCiCd/controller/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.githubcicd.githubActionCiCd.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/welcome")
+public class WelcomeController {
+
+    @GetMapping
+    ResponseEntity<String> welcome (){
+        return ResponseEntity.ok("Welcome to spring boot app");
+    }
+}
---
+++ src/test/java/com/githubcicd/githubActionCiCd/GithubActionCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.githubcicd.githubActionCiCd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
